74c9f3de58555ff274220ab27c2ac91aeea59aa5
Thiago Frazzon Arend <thiago.arend@gmail.com>
2026-02-26 15:20:16 -0300

    chore: spring initializer configs

---
+++ src/main/java/com/thiagoarend/parking_spot_management_api/ParkingSpotManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoarend.parking_spot_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpotManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSpotManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagoarend/parking_spot_management_api/ParkingSpotManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoarend.parking_spot_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpotManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
