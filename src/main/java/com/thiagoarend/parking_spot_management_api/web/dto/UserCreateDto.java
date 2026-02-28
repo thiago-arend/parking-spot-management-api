b28258d979028ede57e6c18dcc4a88865067ee0e
Thiago Frazzon Arend <thiago.arend@gmail.com>
2026-02-28 15:35:06 -0300

    feat: add field value validation to dto's

--- src/main/java/com/thiagoarend/parking_spot_management_api/web/dto/UserCreateDto.java
+++ src/main/java/com/thiagoarend/parking_spot_management_api/web/dto/UserCreateDto.java
@@ -1,5 +1,8 @@
 package com.thiagoarend.parking_spot_management_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,10 @@ import lombok.ToString;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UserCreateDto {
+    @NotBlank
+    @Email(regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
--- src/main/java/com/thiagoarend/parking_spot_management_api/web/dto/UserPasswordDto.java
+++ src/main/java/com/thiagoarend/parking_spot_management_api/web/dto/UserPasswordDto.java
@@ -1,5 +1,7 @@
 package com.thiagoarend.parking_spot_management_api.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,13 @@ import lombok.ToString;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UserPasswordDto {
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String currentPassword;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String newPassword;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String confirmPassword;
 }
